e5d59baf39f8edad00e7dd221e7994089948cbe7
Marlon Rodrigues <marlon.gerodrigues@gmail.com>
2017-11-26 09:07:34 -0800

    First commit - Waypoint updater partial implemented

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -2,8 +2,11 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import Pose
 from styx_msgs.msg import Lane, Waypoint
 
+import tf
 import math
 
 '''
@@ -22,6 +25,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+MILECONVERSION = 0.44704 # meter per seconds
 
 
 class WaypointUpdater(object):
@@ -37,16 +41,125 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        
+        # Current pose of the vehicle
+        self.pose = None
+
+        # Part of the complete waypoints retrieved from /waypoints
+        self.waypoints = None
+
+        # Velocity
+        self.current_velocity = None
+
+        # Base waypoints ahead
+        self.waypoints_ahead = []
+
+        # First waypoint ahead
+        self.next_waypoint_id = None
+
+        # Cycle time for the waypoint updater
+        self.cycleTimeUpdater = 0.1
+
+        # Maximum allowed velocity
+        self.max_velocity = 10.0 * MILECONVERSION
+
+        # Minimum distance to the waypoint ahead
+        self.min_distance_ahead = self.max_velocity * self.cycleTimeUpdater
+
+        rospy.Timer(rospy.Duration(self.cycleTimeUpdater), self.process_waypoints)
 
         rospy.spin()
 
+    def process_waypoints(self, event):
+        if not self.pose:
+            rospy.logwarn('no pose has been set')
+            return None
+
+        #if not self.current_velocity:
+        #    rospy.logwarn('no velocity has been set')
+        #    return None
+
+        if not self.waypoints:
+            rospy.logwarn('no waypoints has been set')
+            return None
+
+        # get nearest waypoint
+        begin = self.get_nearest_waypoint()
+        end = begin + LOOKAHEAD_WPS
+        end = min(end, len(self.waypoints))
+        rospy.logdebug("begin {}, end {}, waypoints len {}".format(begin, end, len(self.waypoints)))
+
+        waypointsArr = []
+        epsilon = 1.0
+        velocity = 0
+        previous_target_velocity = 0
+
+        for i in range(begin, end):
+            target_velocity = self.max_velocity
+
+            # accelerate smoothly
+            start_waypoint_velocity = self.get_waypoint_velocity(self.waypoints[begin])
+            previous_target_velocity = start_waypoint_velocity if i == begin else previous_target_velocity
+            curr_waypoint_velocity = self.get_waypoint_velocity(self.waypoints[i])
+
+            rospy.logdebug("{} = start v {}, current v {}, prev v {}, target v {}".format(i, start_waypoint_velocity, curr_waypoint_velocity, previous_target_velocity, target_velocity ))
+
+            if start_waypoint_velocity == 0 and curr_waypoint_velocity == 0 and previous_target_velocity == 0:
+                target_velocity = (0.25 * target_velocity + 0.75 * previous_target_velocity)
+            elif previous_target_velocity < target_velocity:
+                target_velocity = (0.1 * target_velocity + 0.9 * previous_target_velocity)
+
+            # make sure target_velocity doesn't suprass max allowed velocity
+            if target_velocity > 4.0:
+                target_velocity = self.max_velocity
+
+            # make sure target velocity is within bounds 
+            target_velocity = min(max(0, target_velocity), self.max_velocity)
+
+            # store previous target velocity
+            previous_target_velocity = target_velocity
+
+            self.set_waypoint_velocity(self.waypoints, i, target_velocity)
+            waypointsArr.append(self.waypoints[i])
+
+        lane = Lane()
+        lane.waypoints = waypointsArr
+        self.final_waypoints_pub.publish(lane)
+
+        #vd = self.get_waypoint_velocity(self.waypoints[begin])
+            
+    def get_nearest_waypoint(self):
+        # get the pose of the vehicle
+        curr_x = self.pose.position.x
+        curr_y = self.pose.position.y
+        curr_orientation = self.pose.orientation
+        curr_quaternion = (curr_orientation.x, curr_orientation.y, curr_orientation.z, curr_orientation.w)
+        _, _, curr_t = tf.transformations.euler_from_quaternion(curr_quaternion)
+
+        nearest_waypoint = (float('inf'), -1)
+        for waypoint_idx in range(len(self.waypoints)):
+            # compute the euclidian distance of the waypoint from the current pose of the vehicle
+            waypoint_x = self.waypoints[waypoint_idx].pose.pose.position.x
+            waypoint_y = self.waypoints[waypoint_idx].pose.pose.position.y
+            waypoint_ahead = ((waypoint_x - curr_x) * math.cos(curr_t) + 
+                              (waypoint_y - curr_y) * math.sin(curr_t)) > self.min_distance_ahead
+
+            if not waypoint_ahead:
+                continue
+
+            distance = math.sqrt((curr_x - waypoint_x)**2 + (curr_y - waypoint_y)**2)
+            if distance < nearest_waypoint[0]:
+                nearest_waypoint = (distance, waypoint_idx)
+
+        return nearest_waypoint[1]
+
     def pose_cb(self, msg):
         # TODO: Implement
-        pass
+        self.pose = msg.pose
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
-        pass
+        self.waypoints = waypoints.waypoints
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
