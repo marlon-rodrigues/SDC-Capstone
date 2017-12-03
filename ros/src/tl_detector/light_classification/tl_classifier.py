3a325a033ca86a3ef0e329036f87a9a764006046
Marlon Geraldo Rodrigues Viana <marlon.gerodrigues@gmail.com>
2017-12-03 15:54:20 +0000

    Revert "Develop"

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,45 +1,11 @@
 from styx_msgs.msg import TrafficLight
-import keras
-
-import cv2
-import numpy as np
-import rospy
-
-import tensorflow as tf
-from keras.models import load_model
-import yaml
-
-
-
-WIDTH  = 64
-HEIGHT = 64
-
-
-light_label = {
-
-    0 : "RED",
-    1 : "YELLOW",
-    2 : "GREEN"
-}
 
 class TLClassifier(object):
-
-
     def __init__(self):
-
-        config_string = rospy.get_param("/traffic_light_config")
-        config = yaml.load(config_string)
-        model_path = config["model_path"]
-        rospy.logwarn( "Loading error model %s" % model_path)
-
-        self.classifier = load_model(model_path)
-        self.classifier._make_predict_function()
-        self.graph = tf.get_default_graph()
-
-
+        #TODO load classifier
+        pass
 
     def get_classification(self, image):
-
         """Determines the color of the traffic light in the image
 
         Args:
@@ -49,23 +15,5 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #rospy.logwarn( "get get_classification")
-        #return TrafficLight.UNKNOWN
-        new_img = cv2.resize( image , (WIDTH, HEIGHT) ).reshape( 1, WIDTH, HEIGHT, 3)
-        retval = TrafficLight.UNKNOWN
-        with self.graph.as_default():
-            ret = self.classifier.predict(new_img)
-            tl = np.argmax(ret)
-
-            rospy.loginfo( "Trafic Light Classified as {}".format( light_label[tl] ))
-            if tl==0:
-                retval= TrafficLight.RED
-            elif tl==1:
-                retval = TrafficLight.YELLOW
-            elif tl==2:
-                retval = TrafficLight.GREEN
-            else:
-                retval = TrafficLight.UNKNOWN
-
-
-        return retval
+        #TODO implement light color prediction
+        return TrafficLight.UNKNOWN
