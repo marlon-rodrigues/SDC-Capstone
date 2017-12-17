9b21983c181553a88ddbca906f49d0c1bc9ae915
ricgu8086 <ricgu8086@gmail.com>
2017-12-17 19:03:52 +0000

    Continue improving object_detection_training. Bug fixing and better descriptions. Now is fully working (tested).

--- object_detection_training/src/variables.sh
+++ object_detection_training/src/variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Edit the next variable with your working directory for this project
-SDC_WORKING_DIR=/home/ubuntu/SDC/tl_detector
+SDC_WORKING_DIR=/home/ubuntu/SDC/SDC-Capstone/object_detection_training
 
 # Saving to .bashrc to make this variable non-volatile
 echo SDC_WORKING_DIR=$SDC_WORKING_DIR >> ~/.bashrc
+
+source ~/.bashrc
